3e518f1f0b37ae2a87a809dd035b31907ff808a2
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-06-29 17:22:06 +0330

    ✨ feat:Added Header

--- src/containers/layout/Header.tsx
+++ src/containers/layout/Header.tsx
@@ -1,13 +1,47 @@
 import Link from "next/link";
 import React from "react";
+import { RiHome4Line, RiInformation2Line, RiNextjsFill } from "react-icons/ri";
+import { RiBloggerLine } from "react-icons/ri";
 
+const navItems = [
+  {
+    icon: (
+      <RiHome4Line className="w-5 h-5 text-slate-700 group-hover:text-violet-600" />
+    ),
+    label: "خانه",
+  },
+  {
+    icon: (
+      <RiBloggerLine className="w-5 h-5 text-slate-700 group-hover:text-violet-600" />
+    ),
+    label: "بلاگ ها",
+  },
+  {
+    icon: (
+      <RiInformation2Line className="w-5 h-5 text-slate-700 group-hover:text-violet-600" />
+    ),
+    label: "درباره ما",
+  },
+];
 const Header = () => {
   return (
-    <header className="mb-7">
+    <header className="mb-7 text-slate-800">
       <div className="container mx-auto xl:max-w-screen-xl flex items-center justify-between py-4">
-        <div className="flex gap-x-5">
-          <Link href="/">خانه</Link>
-          <Link href="/blogs">بلاگ ها</Link>
+        <div className="flex gap-x-9 items-center">
+          <div className="flex items-center gap-x-1 text-violet-500">
+            <RiNextjsFill className="w-10 h-10" />
+            <span className="font-black text-lg">بلاگـ شاپـ</span>
+          </div>
+
+          {navItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center cursor-pointer gap-x-1 group hover:text-violet-600"
+            >
+              {item.icon}
+              <Link href="/blogs">{item.label}</Link>
+            </div>
+          ))}
         </div>
 
         <div className="flex gap-x-5 items-center">
