4634b77edaa286981ba57da4427b78d8801242de
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-06-29 09:20:33 +0330

    🌈 style:Custom font-Vazir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className="bg-green-200">welcome</main>;
+  return <main className="bg-green-200">سلام</main>;
 }
