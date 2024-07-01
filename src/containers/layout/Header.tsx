917fa45900b8a8340b1c6456e401b9001e2137bf
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-07-01 10:33:35 +0330

    🌈 style:Updayed Styles

--- src/containers/layout/Header.tsx
+++ src/containers/layout/Header.tsx
@@ -6,21 +6,21 @@ import { RiBloggerLine } from "react-icons/ri";
 const navItems = [
   {
     icon: (
-      <RiHome4Line className="w-5 h-5 text-slate-700 group-hover:text-violet-600" />
+      <RiHome4Line className="w-5 h-5 text-slate-700 group-hover:text-violet-700" />
     ),
     label: "خانه",
     href: "/",
   },
   {
     icon: (
-      <RiBloggerLine className="w-5 h-5 text-slate-700 group-hover:text-violet-600" />
+      <RiBloggerLine className="w-5 h-5 text-slate-700 group-hover:text-violet-700" />
     ),
     label: "بلاگ ها",
     href: "/blogs",
   },
   {
     icon: (
-      <RiInformation2Line className="w-5 h-5 text-slate-700 group-hover:text-violet-600" />
+      <RiInformation2Line className="w-5 h-5 text-slate-700 group-hover:text-violet-700" />
     ),
     label: "درباره ما",
     href: "/about-us",
@@ -31,7 +31,7 @@ const Header = () => {
     <header className="mb-7 bg-[#f4f5f7] z-10 text-slate-800 sticky top-0">
       <div className="container mx-auto xl:max-w-screen-xl flex items-center justify-between py-4">
         <div className="flex gap-x-9 items-center">
-          <Link href="/" className="flex items-center gap-x-1 text-violet-500">
+          <Link href="/" className="flex items-center gap-x-1 text-violet-700">
             <RiNextjsFill className="w-10 h-10" />
             <span className="font-black text-lg">بلاگـ شاپـ</span>
           </Link>
@@ -40,7 +40,7 @@ const Header = () => {
             <Link
               href={item.href}
               key={index}
-              className="flex items-center cursor-pointer gap-x-1 group hover:text-violet-600"
+              className="flex transition-all duration-200 items-center cursor-pointer gap-x-1 group hover:text-violet-700"
             >
               {item.icon}
               <span>{item.label}</span>
@@ -51,7 +51,7 @@ const Header = () => {
         <div className="flex gap-x-5 items-center">
           <Link href="/signup">ثبت نام</Link>
           <Link
-            className="bg-violet-500 hover:bg-violet-400 transition-all duration-200 text-violet-100 px-5 py-2 rounded-lg"
+            className="bg-violet-700 hover:bg-violet-500 transition-all duration-200 text-violet-100 px-5 py-2 rounded-lg"
             href="/signin"
           >
             ورود
--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,7 +1,27 @@
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import React from "react";
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div className="bg-[#f4f5f7]  text-slate-800 min-h-screen py-11">
+      <div className="md:max-w-screen-md container mx-auto shadow-sm p-2 rounded-md bg-white border">
+        <div className="flex items-center mb-9">
+          <Link
+            href="/"
+            className="flex items-center transition-all duration-300 gap-x-1 bg-violet-600 text-white hover:bg-violet-200 hover:text-violet-800 rounded-lg p-1"
+          >
+            <ArrowLongRightIcon className="w-6 h-6" />
+            <span>برگشت</span>
+          </Link>
+        </div>
+        <p>
+          پروژه فروشگاهی-بلاگی هست که با نکست و تایپ اسکریپت توسعه داده شده.
+        </p>
+        <span>...</span>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
